04493f5cbc0e2b5bfcde1e8172afa69eb4088bf3
martinL0918 <martin641379@gmail.com>
2020-09-29 18:53:55 +0800

    Added a button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button aButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aButton = (Button) findViewById(R.id.aButton);
     }
 }
